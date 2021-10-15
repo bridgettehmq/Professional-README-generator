46b520838595a47441d7f011ae4e14d0faa2f1e6
Bridgette Quiambao <bridgettequiambao@gmail.com>
2021-10-14 20:07:36 -0700

    homework

---
+++ bridgette/Bridgette'sREADME.md
@@ -0,0 +1,28 @@
+
+# Bridgette's Fun README Generator
+    
+# Table of Contents
+1. [Description](#description)
+2. [Installation](#installation)
+3. [Usage Information](#usage)
+4. [Guidelines](#guidelines)
+5. [Instructions](#instructions)
+6. [License](#license)
+7. [Questions](#questions)
+## Project Description
+* In this project, I created a README generator using node.js
+## Installation Instructions
+* First I installed inquirer the I wrote the prompts 
+## Usage Information
+* undefined
+## Contributor Guidelines
+* I followed the guidelines found on UCSDbootcamp's GitLab page
+## Test Instructions
+* first use command line then follow the prompts
+## Test Instructions
+* undefined
+## License
+* This application is covered under: no license
+## Questions
+* You can find my github profile here: [bridgettequiambao](http://github.com/bridgettequiambao)
+* If you have any additional questions, you can reach me at bridgettequiambao@gmail.com
---
+++ script.js
@@ -0,0 +1,112 @@
+const inquirer = require("inquirer");
+
+const fs = require("fs");
+
+  const questions = [
+    {
+      type: "input",
+      name: "Title",
+      message: "What is the title of your project?"
+    },
+
+    {
+      type: "input",
+      name: "description",
+      message: "Provide a short description explaining the what, why, and how of your project."
+    },
+
+    {
+      type: "input",
+      name: "installation",
+      message: "What are the steps required to install your  project?"
+    },
+        
+    {
+      type: "input",
+      name: "usage",
+      message: "Please provide information about your project's usage."
+    },
+
+    {
+      type: "input",
+      name: "guidelines",
+      message: "Please provide contribution guidelines."
+    },
+
+    {
+      type: "input",
+      name: "instructions",
+      message: "Please provide test instructions."
+    },
+    
+    {
+      type: "input",
+      name: "license",
+      message: "License?"
+    },
+    
+    {
+      type: "input",
+      name: "github",
+      message: "Enter your github username"
+    },
+    
+    {
+      type: "input",
+      name: "email",
+      message: "Enter your email account"
+    },
+
+  ];
+
+function generateREADME(data) {
+  return `
+# ${data.Title}
+    
+# Table of Contents
+1. [Description](#description)
+2. [Installation](#installation)
+3. [Usage Information](#usage)
+4. [Guidelines](#guidelines)
+5. [Instructions](#instructions)
+6. [License](#license)
+7. [Questions](#questions)
+## Project Description
+* ${data.description}
+## Installation Instructions
+* ${data.installation}
+## Usage Information
+* ${data.information}
+## Contributor Guidelines
+* ${data.guidelines}
+## Test Instructions
+* ${data.instructions}
+## Test Instructions
+* ${data.test}
+## License
+* This application is covered under: ${data.license}
+## Questions
+* You can find my github profile here: [${data.github}](http://github.com/${data.github})
+* If you have any additional questions, you can reach me at ${data.email}`;
+  
+}
+
+function writeToFile(filename, data) {
+
+  fs.writeFile('./bridgette/'+filename, data, (err) =>
+    err? console.log(err) : console.log("Success! Your README.md has been created!")
+  );
+
+}
+
+
+  function init () {
+
+    inquirer.prompt(questions)
+    .then(function (data) {
+      writeToFile("Bridgette'sREADME.md", generateREADME(data));
+    })
+
+  }
+
+  init();
